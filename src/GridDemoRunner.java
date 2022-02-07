03c8f5e91229588921af4d6e6c3df34923bb23a3
Grace <GraceStephensPersonal@protonmail.com>
2022-02-07 15:04:01 -0600

    my edit

--- src/GridDemoRunner.java
+++ src/GridDemoRunner.java
@@ -3,12 +3,13 @@ public class GridDemoRunner
 
 	public static void main(String[] args)
 	{
+		//TODO *add list of stuff for us to do here*
 
 		// by default, we're using the StartHereFrame so you can get used to the Cell class;
 		// switch the commenting between these lines when you are ready for the full program.
 
 		StartHereFrame practiceApp = new StartHereFrame();
-//		GridDemoFrame app = new GridDemoFrame();
+//	GridDemoFrame app = new GridDemoFrame();
 
 
 		// Evan: Test test
