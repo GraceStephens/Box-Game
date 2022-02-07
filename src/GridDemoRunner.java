6fda51810dae06c3e345613ed2c15e504b3eabf0
Grace <GraceStephensPersonal@protonmail.com>
2022-02-07 15:08:34 -0600

    goals?

--- src/GridDemoRunner.java
+++ src/GridDemoRunner.java
@@ -3,7 +3,10 @@ public class GridDemoRunner
 
 	public static void main(String[] args)
 	{
-		//TODO *add list of stuff for us to do here*
+		//goals:
+		//TODO make a 2 phase maze
+		//features:
+		//TODO triggers, "lights off" effect, coins, step tracker
 
 		// by default, we're using the StartHereFrame so you can get used to the Cell class;
 		// switch the commenting between these lines when you are ready for the full program.
